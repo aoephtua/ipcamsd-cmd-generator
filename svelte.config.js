423bb4ad843dfca7e6fa5d487080ccac9dac5125
aoephtua <aoephtua@mailo.com>
2022-05-17 15:12:09 +0200

    Updates NPM modules to latest releases

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,9 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
 	kit: {
 		adapter: adapter(),
-		target: '#svelte'
+		prerender: {
+			default: true
+		}
 	},
 	preprocess: preprocess()
 };
